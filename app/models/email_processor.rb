a6d24a37dc2b53437d004a3a002961672addb323
rittersm <rittersm@gmail.com>
2016-08-02 23:38:49 -0400

    fixed mailer

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -7,7 +7,7 @@ class EmailProcessor
   def process
     photo = Photo.create!({ caption: @email.subject, from_email: @email.from[:email], photo: @email.attachments.first })
     Pusher.trigger('HookEm', 'new_photo', PhotoSerializer.new(photo).as_json)
-    SubmissionMailer.submission_email(@photo).deliver_later
+    SubmissionMailer.submission_email(photo).deliver_now
   end
 
 end
